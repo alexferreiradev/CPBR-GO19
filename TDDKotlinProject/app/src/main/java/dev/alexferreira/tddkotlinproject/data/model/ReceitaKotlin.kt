556860065bd15b2f1aef1fef545db4afc0c54120
Alex Rabelo Ferreira <arf92@live.com>
2019-09-05 12:16:30 -0300

    add de telas auxiliares de testes

---
+++ TDDKotlinProject/app/src/main/java/dev/alexferreira/tddkotlinproject/data/model/ReceitaKotlin.kt
@@ -0,0 +1,8 @@
+package dev.alexferreira.tddkotlinproject.data.model
+
+data class ReceitaKotlin(
+    val id: Long,
+    val nome:String,
+    val descricao:String,
+    val link:String
+)
---
+++ TDDKotlinProject/app/src/main/java/dev/alexferreira/tddkotlinproject/ui/view/AdicioneReceitaActivity.kt
@@ -0,0 +1,7 @@
+package dev.alexferreira.tddkotlinproject.ui.view
+
+import android.support.v7.app.AppCompatActivity
+
+class AdicioneReceitaActivity: AppCompatActivity(){
+
+}
